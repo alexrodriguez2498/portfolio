9789bbec6c9ad0eafc0672cdba0ef9ba2bc2a5c5
alex rodriguez <alexcarvajal2404@gmail.com>
2021-02-05 09:35:15 -0500

    adding files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,5 @@
 /**
  * Implement Gatsby's Browser APIs in this file.
  *
-<<<<<<< HEAD
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-=======
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
->>>>>>> main
